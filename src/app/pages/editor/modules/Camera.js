b57b127598c632c933c39f5c9c31a1547eceadc9
Disorder <disorrder@gmail.com>
2018-06-01 01:34:54 +0300

    camera look around and zoom, WIP look orbit, translate

--- src/app/pages/editor/modules/Camera.js
+++ src/app/pages/editor/modules/Camera.js
@@ -1,3 +1,5 @@
+var vec3 = new pc.Vec3();
+
 export default class Camera {
     constructor(app) {
         this.app = app;
@@ -5,6 +7,7 @@ export default class Camera {
         this.attachEvents();
 
         this.moving = false;
+        this.sensitivity = 0.2;
     }
 
     createEntity() {
@@ -18,43 +21,89 @@ export default class Camera {
 
     get camera() { return this.entity.camera; }
 
+    get pitch() { return this._pitch || 0; }
+    set pitch(val) { this._pitch = pc.math.clamp(val, -90, 90); }
+    get yaw() { return this._yaw || 0; }
+    set yaw(val) { this._yaw = val; }
+
     attachEvents() {
+        // this.app.on('update', this.update, this);
         this.app.mouse.on(pc.EVENT_MOUSEDOWN, this.onMouseDown, this);
         this.app.mouse.on(pc.EVENT_MOUSEUP, this.onMouseUp, this);
         // this.app.mouse.on(pc.EVENT_MOUSEMOVE, this.onMouseMove, this);
+        this.app.mouse.on(pc.EVENT_MOUSEWHEEL, this.onMouseWheel, this);
     }
 
     onMouseDown(e) {
-        console.log('MD', e);
+        // console.log('MD', e);
         if (this.moving) return;
+        // if (this.moving) {
+        //     $(document).off('mousemove.camera');
+        // };
         this.moving = true;
         this._startEvent = e;
         $(document).on('mousemove.camera', this.onMouseMove.bind(this));
     }
 
     onMouseUp(e) {
-        console.log('MU', e);
+        // console.log('MU', e);
         if (!this.moving) return;
         this.moving = false;
         this._startEvent = null;
+        this._prevEvent = null;
         $(document).off('mousemove.camera');
     }
 
     onMouseMove(e) {
-        // var btns = [0, 1, 2].map((v) => this.app.mouse.isPressed(v));
         console.log('MM', e);
+        if (!this._prevEvent) {
+            this._prevEvent = e;
+            return;
+        };
+        let dx = e.clientX - this._prevEvent.clientX;
+        let dy = e.clientY - this._prevEvent.clientY;
+        this._prevEvent = e;
 
-        if (this.app.mouse.isPressed(pc.MOUSEBUTTON_LEFT)) {
-            return this.lookAround(e);
+        let [LMB, MMB, RMB] = this.app.mouse._buttons;
+
+        if (e.shiftKey && LMB || LMB && RMB || MMB) {
+            return this.translate(dx, dy);
+        }
+        if (LMB) { // this.app.mouse.isPressed(pc.MOUSEBUTTON_LEFT)
+            return this.lookAround(dx, dy);
+        }
+        if (RMB) { // this.app.mouse.isPressed(pc.MOUSEBUTTON_RIGHT)
+            return this.lookOrbit(dx, dy);
         }
 
     }
 
-    lookAround(e) {
-        console.log('look around');
+    onMouseWheel(e) {
+        // console.log('MW', e);
+        this.zoom(e.wheel);
     }
 
-    lookOrbit(e) {
+    lookAround(dx, dy) {
+        if (typeof dx === 'object') var e = dx;
+        this.pitch -= dy * this.sensitivity;
+        this.yaw -= dx * this.sensitivity;
+        this.entity.setLocalEulerAngles(this.pitch, this.yaw, 0);
+    }
+
+    lookOrbit(dx, dy) {
 
     }
+
+    zoom(wheel) {
+        vec3.copy(this.entity.forward).scale(wheel*this.sensitivity);
+        this.entity.translate(vec3);
+    }
+
+    translate(dx, dy) {
+        console.log('translate', dx, dy);
+    }
+
+    // update() {
+    //
+    // }
 }
